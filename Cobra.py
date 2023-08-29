f45d6adeefd5dd9a6e366119951221b13a6b393c
Jacques Bilombe <64991873+james988@users.noreply.github.com>
2023-08-29 16:54:12 -0300

    Update Cobra.py

--- Cobra.py
+++ Cobra.py
@@ -1,7 +1,7 @@
 # Nome do Projeto: COBRA (Congo-Brasil)
 # Áreas de estudo: Processamento de Sinais (Imagens), Tratamento de Materias (Metalurgia)
 # Tipo de Projeto: OpenSource mas com foco CEFET-RJ (Maracanã)
-# Repositório do Projeto:
+# Repositório do Projeto: https://github.com/james988/Cobra_EstudoDeMateriais
 # Ingenheiros Fundadores do Projeto: Carlos Eduardo Guedes Catunda e Jacques Ben Bilombe 
 
 from Analyzer import processing
@@ -9,10 +9,11 @@ from Analyzer import processing
 def main():
     # Ask the user if He/She wants extract a ROI or to analyze the entire Image.
     ROI_NOT = input('Escolha a processo de análise: Y/y para imagem inteira ou N/n para selecionar uma parte: ')
-    flag = True if input('\n Selecione D para Default or C para ROI Personalizado: ') == 'D' else False
+    flag = True if input('\nSelecione D para Default or C para ROI Personalizado: ') == 'D' else False
 
     processing(ROI_NOT, flag)
 
 
 if __name__ == '__main__':
+    print('Start the analyze\n')
     main()
